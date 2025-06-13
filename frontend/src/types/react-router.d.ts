a058f0a5e2f6842ee892567cec57cb315b769783
jantoniogarciah <joseantonio.garciah@yahoo.com>
2025-06-12 22:59:09 -0600

    Correccion Errores Router REact

--- frontend/src/types/react-router.d.ts
+++ frontend/src/types/react-router.d.ts
@@ -1,11 +1,30 @@
 import 'react-router-dom';
+import { RouteProps as DefaultRouteProps } from 'react-router-dom';
 
 declare module 'react-router-dom' {
-  export interface RouteProps {
+  export interface RouteProps extends Omit<DefaultRouteProps, 'element'> {
     caseSensitive?: boolean;
     children?: React.ReactNode;
     element?: React.ReactNode | null;
     index?: boolean;
     path?: string;
+  }
+
+  export interface StaticRouterProps {
+    basename?: string;
+    children?: React.ReactNode;
+    location?: Partial<Location> | string;
+  }
+
+  export interface SwitchProps {
+    children?: React.ReactNode;
+    location?: Location;
+  }
+
+  export interface match<Params extends { [K in keyof Params]?: string } = {}> {
+    params: Params;
+    isExact: boolean;
+    path: string;
+    url: string;
   }
 } 
